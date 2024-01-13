c257508ec26c2321d54a0eab3e33e080a139143a
Adam <897017+aSemy@users.noreply.github.com>
2024-01-13 13:30:18 +0100

    tidy kotlin-gradle-plugin convention, add Javadoc readme.txt

--- buildSrc/src/main/kotlin/buildsrc/conventions/kotlin-gradle-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/conventions/kotlin-gradle-plugin.gradle.kts
@@ -5,87 +5,45 @@ plugins {
   id("buildsrc.conventions.java-base")
   id("org.gradle.kotlin.kotlin-dsl")
   id("com.gradle.plugin-publish")
-  `maven-publish`
 }
 
 tasks.validatePlugins {
   enableStricterValidation = true
 }
 
-sourceSets {
-  configureEach {
-    java.setSrcDirs(emptyList<File>())
+val createJavadocJarReadme by tasks.registering(Sync::class) {
+  description = "generate a readme.txt for the Javadoc JAR"
+  val projectCoords = provider { project.run { "$group:$name:$version" } }
+  inputs.property("projectGAV", projectCoords)
+  val projectCoordsToken = "%{projectGAV}"
+  from(
+    resources.text.fromString(
+      """
+      |This Javadoc JAR for $projectCoordsToken is intentionally empty.
+      |
+      |For documentation, see the sources JAR or https://github.com/adamko-dev/kotlin-binary-compatibility-validator-mu/
+      |
+      """.trimMargin()
+    )
+  ) {
+    rename { "readme.txt" }
+  }
+  into(temporaryDir)
+  doLast {
+    temporaryDir.walk()
+      .filter { it.isFile }
+      .forEach { file ->
+        file.writeText(
+          file.readText().replace(projectCoordsToken, projectCoords.get())
+        )
+      }
   }
 }
 
-//
-///** These dependencies will be provided by Gradle, and we should prevent version conflict */
-//fun Configuration.excludeGradleCommonDependencies() {
-//  dependencies
-//    .withType<ModuleDependency>()
-//    .configureEach {
-//      exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
-//      exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
-//      exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
-//      exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-common")
-//      exclude(group = "org.jetbrains.kotlin", module = "kotlin-reflect")
-//      exclude(group = "org.jetbrains.kotlin", module = "kotlin-script-runtime")
-//    }
-//}
-//
-//// Exclude Gradle runtime from given SourceSet configurations
-////configurations[sourceSet.implementationConfigurationName].excludeGradleCommonDependencies()
-////configurations[sourceSet.apiConfigurationName].excludeGradleCommonDependencies()
-////configurations[sourceSet.runtimeOnlyConfigurationName].excludeGradleCommonDependencies()
-//
-//dependencies {
-//  constraints {
-//
-//  }
-//}
-//
-//abstract class AsmCapability : ComponentMetadataRule {
-//}
-//
-//@CacheableRule
-//abstract class GradleKotlinLibsCapability () : ComponentMetadataRule {
-//
-//  private val kotlinGroup = "org.jetbrains.kotlin"
-//  private val kotlinLibs = setOf(
-//    "kotlin-stdlib",
-//  "kotlin-stdlib-jdk7",
-//  "kotlin-stdlib-jdk8",
-//  "kotlin-stdlib-common",
-//  "kotlin-reflect",
-//  "kotlin-script-runtime",
-//  )
-//
-//  override fun execute(context: ComponentMetadataContext) = context.details.run {
-//    if (id.group == kotlinGroup && id.name in kotlinLibs) {
-//      allVariants {
-//        withCapabilities {
-//          addCapability(kotlinGroup, "gradle-kotlin-embedded", id.version)
-//        }
-//      }
-//    }
-//  }
-//  override fun execute(context: ComponentMetadataContext) { context.details.withVariant("compile") {
-//      attributes {
-//        attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, jvmVersion)
-//        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_API))
-//      }
-//    }
-//  }
-//}
-//dependencies {
-//  components {
-//    withModule<TargetJvmVersionRule>("commons-io:commons-io") {
-//      params(7)
-//    }
-//    withModule<TargetJvmVersionRule>("commons-collections:commons-collections") {
-//      params(8)
-//    }
-//  }
-//  implementation("commons-io:commons-io:2.6")
-//  implementation("commons-collections:commons-collections:3.2.2")
-//}
+
+// The Gradle Publish Plugin enables the Javadoc JAR in afterEvaluate, so find it lazily
+tasks.withType<Jar>()
+  .matching { it.name == "javadocJar" }
+  .configureEach {
+    from(createJavadocJarReadme)
+  }
