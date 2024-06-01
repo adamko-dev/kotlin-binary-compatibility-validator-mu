37762e10ad90c8da498f8c538cfd175006271ef4
Adam <897017+aSemy@users.noreply.github.com>
2024-06-01 17:18:35 +0200

    tidy kdoc

--- modules/bcv-gradle-plugin/src/main/kotlin/targets/KLibValidationSpec.kt
+++ modules/bcv-gradle-plugin/src/main/kotlin/targets/KLibValidationSpec.kt
@@ -25,12 +25,12 @@ interface KLibValidationSpec {
    *
    * This option covers some advanced scenarios and does not require any configuration by default.
    *
-   * A linker uses signatures to look up symbols, thus signature changes brake binary compatibility and
-   * should be tracked. Signature format itself is not stabilized yet and may change in the future. In that case,
-   * a new version of a signature will be introduced. Change of a signature version will be reflected in a dump
-   * causing a validation failure even if declarations itself remained unchanged.
-   * However, if a KLib supports multiple signature versions simultaneously, one my explicitly specify the version
-   * that will be dumped to prevent changes in a dump file.
+   * A linker uses signatures to look up symbols, thus signature changes brake binary compatibility
+   * and should be tracked. Signature format itself is not stabilized yet and may change in the
+   * future. In that case, a new version of a signature will be introduced. Change of a signature
+   * version will be reflected in a dump causing a validation failure even if declarations itself
+   * remained unchanged. However, if a KLib supports multiple signature versions simultaneously,
+   * one may explicitly specify the version that will be dumped to prevent changes in a dump file.
    */
   @get:Input
   @get:Optional
@@ -45,8 +45,9 @@ interface KLibValidationSpec {
 
   /**
    * Fail validation if some build targets are not supported by the host compiler.
-   * By default, ABI dumped only for supported files will be validated. This option makes validation behavior
-   * stricter and treats having unsupported targets as an error.
+   *
+   * By default, ABI dumped only for supported files will be validated. This option makes
+   * validation behavior stricter and treats having unsupported targets as an error.
    */
   @get:Input
   @get:Optional
