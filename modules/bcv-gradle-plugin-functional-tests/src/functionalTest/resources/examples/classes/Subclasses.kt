ebd183cc194fd549a58eb6427e9b2fb25a9e766d
Adam <897017+aSemy@users.noreply.github.com>
2024-07-16 00:01:07 +0200

    tidy

--- modules/bcv-gradle-plugin-functional-tests/src/functionalTest/resources/examples/classes/Subclasses.kt
+++ modules/bcv-gradle-plugin-functional-tests/src/functionalTest/resources/examples/classes/Subclasses.kt
@@ -1,8 +1,3 @@
-/*
- * Copyright 2016-2023 JetBrains s.r.o.
- * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
- */
-
 package subclasses
 
 public class A {
